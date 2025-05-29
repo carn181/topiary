c77a233c962d5c8ec44c73a696f4591831ac965c
carn181 <carn181@proton.me>
2025-05-29 14:07:23 -0400

    Add Better Faust Examples

--- topiary-queries/queries/faust.scm
+++ topiary-queries/queries/faust.scm
@@ -1,1 +1,31 @@
-[(program)] @leaf
+[(comment)] @append_hardline
+
+(global_metadata) @leaf
+(function_metadata) @leaf
+(parameters) @leaf
+[(string) (fstring)] @leaf
+
+
+((function_metadata)
+  .
+  ";"
+  @append_hardline) @leaf
+
+
+["with" 
+ "=" 
+ ] @prepend_space @append_space
+
+
+["{" ","] @append_space
+
+(with_environment
+  "with" @prepend_hardline @prepend_indent_start
+  local_environment: 
+  (environment
+    "{" @append_indent_start
+    "}" @append_indent_end
+  )
+)
+
+[(function_definition) (definition) ";" @append_hardline]
